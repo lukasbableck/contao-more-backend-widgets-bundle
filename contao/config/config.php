25870cfc675eb4513324b6368131d21d69c70c67
Lukas Bableck <lukas@bableck.dev>
2024-10-29 12:35:25 +0100

    fix error

--- contao/config/config.php
+++ contao/config/config.php
@@ -3,11 +3,12 @@
 use Contao\System;
 use Lukasbableck\ContaoMoreBackendWidgetsBundle\Widget\Backend\NumberWidget;
 use Lukasbableck\ContaoMoreBackendWidgetsBundle\Widget\Backend\RangeWidget;
+use Symfony\Component\HttpFoundation\Request;
 
 $GLOBALS['BE_FFL']['number'] = NumberWidget::class;
 $GLOBALS['BE_FFL']['range'] = RangeWidget::class;
 
-$route = System::getContainer()->get('request_stack')->getCurrentRequest()->get('_route');
-if ('contao_backend' === $route) {
+
+if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
 	$GLOBALS['TL_CSS'][] = 'bundles/contaomorebackendwidgets/backend.css';
 }
